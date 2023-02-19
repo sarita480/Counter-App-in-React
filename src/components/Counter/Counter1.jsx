77ff14d217e5214fb1408e76e06dcb8f3c8c80fb
sarita <adhsarita24@gmail.com>
2023-02-19 01:35:46 -0800

    add props

--- src/components/Counter/Counter1.jsx
+++ src/components/Counter/Counter1.jsx
@@ -3,16 +3,34 @@ import './Counter.css'
 import propsTypes from 'prop-types'
 
 class Counter extends Component{
+
+    constructor()
+    {
+        super();
+        this.state=
+        {
+            counter1 : 0
+        }
+        this.increment =this.increment.bind(this)
+    }
     render() {
         return (
           <div className="Counter">
-          <CounterButton by={1}></CounterButton>
-          <CounterButton by={5}></CounterButton>
-          <CounterButton by ={10}></CounterButton>
+          <CounterButton by={1} incrementMethod={this.increment}></CounterButton>
+          <CounterButton by={5} incrementMethod={this.increment}></CounterButton>
+          <CounterButton by ={10} incrementMethod={this.increment}></CounterButton>
           </div>
         
           
         );
+}
+increment(by)
+{
+    this.setState({
+       counter1:this.state.counter1+by
+   //  console.log(`Increment from parent -${by}`)
+     
+})
 }
 }
 
@@ -43,7 +61,9 @@ class CounterButton extends Component{
     {
         this.setState({
             counter1:this.state.counter1+this.props.by
-    })
+
+    });
+    this.props.incrementMethod(this.props.by)
     }
 }
 Counter.defaultProps={
